292d459fc25537293c7542dba6a841dd5bd74047
lucaassp <lucaspereira.student@gmail.com>
2022-07-07 22:07:28 -0300

    Reference of posts

--- src/main/java/com/ayty/workshopmongo/config/Instantiation.java
+++ src/main/java/com/ayty/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.ayty.workshopmongo.domain.Post;
 import com.ayty.workshopmongo.domain.User;
+import com.ayty.workshopmongo.dto.AuthorDTO;
 import com.ayty.workshopmongo.repository.PostRepository;
 import com.ayty.workshopmongo.repository.UserRepository;
 
@@ -35,11 +36,16 @@ public class Instantiation implements CommandLineRunner {
 		User marcelo = new User(null, "Marcelo Silva", "marcelo@gmail.com");
 		User luiz = new User(null, "Luiz araujo", "luiz@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("01/07/2022"), "Good trip", "I will go to the capital!", lucas);
-		Post post2 = new Post(null, sdf.parse("22/06/2022"), "Good morning!", "I woke up very happy!", lucas);
+        userReposiroty.saveAll(Arrays.asList(lucas, marcelo, luiz));
+		
+        Post post1 = new Post(null, sdf.parse("01/07/2022"), "Good trip", "I will go to the capital!", new AuthorDTO(lucas));
+		Post post2 = new Post(null, sdf.parse("22/06/2022"), "Good morning!", "I woke up very happy!", new AuthorDTO(lucas));
 
-		userReposiroty.saveAll(Arrays.asList(lucas, marcelo, luiz));
+		
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
+
+        lucas.getPosts().addAll(Arrays.asList(post1, post2));
+        userReposiroty.save(lucas);
 	}
 
 }
